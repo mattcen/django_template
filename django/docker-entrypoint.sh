ead082256a7acde5aee0375c69269c37c4a36d7b
Matthew Cengia <mattcen@mattcen.com>
2021-07-07 17:37:20 +1000

    Switch to using nginx proxy to serve static files

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Performing Django database migrations (if any)"
-python manage.py migrate
+python manage.py migrate --no-input
+
+echo "Copying static files"
+python manage.py collectstatic --no-input --clear
 
 # Creat a super user
 python manage.py shell -c 'from scripts import createsuperuser; createsuperuser.run()'
