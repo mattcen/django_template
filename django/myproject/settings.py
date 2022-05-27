493345db40adb2c1572abff1c72f553a4da3f669
Matthew Cengia <mattcen@mattcen.com>
2022-05-27 13:25:07 +1000

    Set default CSRF_TRUSTED_ORIGINS

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -38,6 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS').split(',')
 
 
 # Application definition
