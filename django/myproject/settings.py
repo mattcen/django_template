0e7ae15e04015f7ebb9b521f42f1efa7bd1b6812
Matthew Cengia <mattcen@mattcen.com>
2022-05-27 13:53:42 +1000

    Add media files dir

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
