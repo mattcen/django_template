71e0cf4fc5253955c3b2e1224b036efc4b29911a
Matthew Cengia <mattcen@mattcen.com>
2022-12-29 00:01:27 +1100

    Correctly use chosen User model in createsuperuser.py

--- django/scripts/createsuperuser.py
+++ django/scripts/createsuperuser.py
@@ -1,4 +1,5 @@
 from os import environ
+from django.contrib.auth import get_user_model
 
 
 def run():
@@ -6,9 +7,8 @@ def run():
     super_password = environ['SUPERUSER_PASSWORD']
     super_email = environ['SUPERUSER_EMAIL']
 
+    User = get_user_model()
     if (super_username != '' and super_password != '' and super_email != ''):
-        from django.contrib.auth import get_user_model
-        from django.contrib.auth.models import User
         has_superuser = False
         for u in User.objects.values():
             if u['is_superuser']:
@@ -16,7 +16,6 @@ def run():
                 break
         if not has_superuser:
             print("Creating super user: " + super_username)
-            User = get_user_model()
             User.objects.create_superuser(
                 super_username, super_email, super_password)
         else:
