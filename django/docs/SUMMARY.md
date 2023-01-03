7362f47e4df8eee38a2eb49c570c972d34cd90ac
Matthew Cengia <mattcen@mattcen.com>
2023-01-03 19:01:24 +1100

    Add basic documentation template served at /docs/

---
+++ django/docs/SUMMARY.md
@@ -0,0 +1,5 @@
+# Summary
+
+[Introduction](00_introduction.md)
+
+- [Chapter 1](01_chapter_1.md)
--- django/myproject/urls.py
+++ django/myproject/urls.py
@@ -49,3 +49,4 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static('docs/', document_root='/docs', show_indexes=True)
