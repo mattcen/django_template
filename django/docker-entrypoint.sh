4c18eb6ba5d4536f52bb6191847464cd7217b743
Matthew Cengia <mattcen@mattcen.com>
2025-06-14 23:09:03 +1000

    Front-load static file collection

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -3,12 +3,6 @@
 echo "Performing Django database migrations (if any)"
 python manage.py migrate --no-input
 
-echo "Copying static files"
-python manage.py collectstatic --no-input --clear
-echo "Copying docs"
-rm -rf /docs/*
-cp -a /built_docs/* /docs/
-
 # Creat a super user
 python manage.py shell -c 'from scripts import createsuperuser; createsuperuser.run()'
 
