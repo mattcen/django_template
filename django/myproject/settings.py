87b8992cd0a632e44e51ddf32bb26794773701f8
Matthew Cengia <mattcen@mattcen.com>
2025-06-15 09:57:17 +1000

    Add default storage backend

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -88,6 +88,9 @@ MIDDLEWARE = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
